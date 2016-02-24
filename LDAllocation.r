a621b7eafd35924d697ed6a3a676064ce113192c
mamonu <thorosm@msn.com>
2016-02-24 16:52:19 +0000

    added stemming.Added saving fitted models comment

--- LDAllocation.r
+++ LDAllocation.r
@@ -7,6 +7,12 @@ library(LDAvis)
 library(slam)
 library(lda)
 library(RJSONIO)
+library(ddR)
+
+nInst = 4 # Change level of parallelism
+useBackend(parallel,executors = nInst)
+
+
 
 
 
@@ -25,36 +31,75 @@ Afile<-'caravanelse.csv'
 data <- read.csv(Afile, stringsAsFactors=FALSE)
 
 desc <- data$description
+
+
+
 desc <- gsub("'", "", desc)  # remove apostrophes
 desc <- gsub("[[:punct:]]", "", desc)  # replace punctuation with space
 desc <- gsub("[[:cntrl:]]", "", desc)  # replace control characters with space
 desc <- gsub("[[:digit:]]+", "", desc) # remove numbers
 
 desc <- gsub("^[[:space:]]+", "", desc) # remove whitespace at beginning of documents
-esc <- gsub("[[:space:]]+$", "", desc) # remove whitespace at end of documents
+desc <- gsub("[[:space:]]+$", "", desc) # remove whitespace at end of documents
 desc <- tolower(desc)  #
 
 
 
-doc.list <- strsplit(desc, "[[:space:]]+")
 
+t1 <- Sys.time()
+doc.list <- strsplit(desc, "[[:space:]]+")
+stem.list <- dlapply (doc.list, wordStem )
+stem.list <- collect (stem.list)
+t2 <- Sys.time()
+t2 - t1  
+
+
+doc.list <- stem.list
 # compute the table of terms:
 term.table <- table(unlist(doc.list))
 term.table <- sort(term.table, decreasing = TRUE)
 
 # remove terms that are stop words or occur fewer than 5 times:
 stop_words <- stopwords("SMART")
-del <- names(term.table) %in% stop_words | term.table < 5
+del <- names(term.table) %in% stop_words | term.table < 5 
+
 term.table <- term.table[!del]
-vocab <- names(term.table)
 
 
-get.terms <- function(x) {
+####STEMMING GOES HERE
+
+
+##########################
+
+
+
+vocab <-  (names(term.table))
+
+
+
+
+
+
+get.terms <- function(x,vocab) {
   index <- match(x, vocab)
   index <- index[!is.na(index)]
   rbind(as.integer(index - 1), as.integer(rep(1, length(index))))
 }
-documents <- lapply(doc.list, get.terms)
+
+###### simple laplly  
+
+# t1 <- Sys.time()
+# documents <- lapply(doc.list, get.terms,vocab)
+# t2 <- Sys.time()
+# t2 - t1  
+
+###### parallel laplly instead
+
+t1 <- Sys.time()
+documents <- dlapply(doc.list, get.terms,vocab)
+documents <-collect (documents)
+t2 <- Sys.time()
+t2 - t1  
 
 # Compute some statistics related to the data set:
 D <- length(documents)  # number of documents  
@@ -65,8 +110,8 @@ term.frequency <- as.integer(term.table)
 
 
 # MCMC and model tuning parameters:
-K <- 3
-G <- 5000
+K <- 10
+G <- 9000
 alpha <- 0.02
 eta <- 0.02
 
@@ -80,24 +125,41 @@ fit <- lda.collapsed.gibbs.sampler(documents = documents, K = K, vocab = vocab,
                                    compute.log.likelihood = TRUE)
 
 
-
-#fit <- mmsb.collapsed.gibbs.sampler(network, K, num.iterations, alpha,
- #                            beta.prior, initial = NULL, burnin = NULL, trace = 0L)
-
-#lda.cvb0(documents, K, vocab, num.iterations, alpha, eta, trace = 0L)
+t2 <- Sys.time()
+t2 - t1  
 
 
+##################################################################################################
+#CVB0 produces a matrix of topic counts for
+#each document, N td and counts of topics assigned to the different words, N wt . These count
+#matrices are converted to conditional probabilities of interest, the distributions of words
+#given topics, p(w|t), and the distributions of topics given documents, p(t|d).
 
+
+# 
+# fitcvb0 <-lda.cvb0(documents = documents, K = K, vocab = vocab, 
+#                   num.iterations = G, alpha = alpha, 
+#                   eta = eta, trace = 0L)
+
+
+########################################################################################################3
 # fit <-  slda.em(documents, K, vocab, num.e.iterations, num.m.iterations, alpha,
 #         eta, annotations, params, variance, logistic = FALSE, lambda = 10,
 #         regularise = FALSE, method = "sLDA", trace = 0L, MaxNWts=3000)
 
+#fit <- mmsb.collapsed.gibbs.sampler(network, K, num.iterations, alpha,
+#                            beta.prior, initial = NULL, burnin = NULL, trace = 0L)
 
-t2 <- Sys.time()
-t2 - t1  
+
+
+
+
 
 # about 2 minutes on laptop
 # 1.5 hours for the big file.
+
+#save fit object (a list) into rdata so we use at another time without doing all the calcs again!!
+#save(fit,file="12clusterfit.RData")
 
 
 #to access a vocab word : lda_description$vocab[1233] 
